6444906180b0b2d6c7a4d48baca939bf60ff0479
Christopher90434 <christopherbryan.susanto@gmail.com>
2025-11-10 21:21:42 +0700

    "Commit No. 3 – UI Element and Theme"

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -11,10 +11,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -24,9 +22,11 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import com.example.lab_week_09.ui.theme.OnBackgroundItemText
+import com.example.lab_week_09.ui.theme.OnBackgroundTitleText
+import com.example.lab_week_09.ui.theme.PrimaryTextButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -107,7 +107,8 @@ fun HomeContent(
                     modifier = Modifier.fillMaxWidth(),
                     contentAlignment = androidx.compose.ui.Alignment.Center
                 ) {
-                    Text(text = stringResource(id = R.string.enter_item))
+                    //Here, we call the OnBackgroundTitleText UI Element
+                    OnBackgroundTitleText(text = stringResource(id = R.string.enter_item))
                 }
 
                 TextField(
@@ -131,13 +132,11 @@ fun HomeContent(
                     modifier = Modifier.fillMaxWidth(),
                     contentAlignment = androidx.compose.ui.Alignment.Center
                 ) {
-                    Button(onClick = {
-                        //Here, we call the onButtonClick lambda function
-                        //This is so that we can add the inputField value to the listData
-                        //and reset the value of the inputField
+                    //Here, we call the PrimaryTextButton UI Element
+                    PrimaryTextButton(
+                        text = stringResource(id = R.string.button_click)
+                    ) {
                         onButtonClick()
-                    }) {
-                        Text(text = stringResource(id = R.string.button_click))
                     }
                 }
             }
@@ -153,7 +152,8 @@ fun HomeContent(
                     .fillMaxWidth(),
                 contentAlignment = androidx.compose.ui.Alignment.Center
             ) {
-                Text(text = item.name)
+                //Here, we call the OnBackgroundItemText UI Element
+                OnBackgroundItemText(text = item.name)
             }
         }
     }
